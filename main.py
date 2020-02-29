cb3adf0181602a30681125de5792d94c09e639ac
Alan Ionita <a.g.ionita@gmail.com>
2020-02-29 20:26:05 +0000

    FIX : updates the implementation of the new api.py file and function and also introduced new dropdown inputs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 from pprint import pprint as pp
 from flask import Flask, flash, redirect, render_template, request, url_for
-from weather import query_api
+from api import get_weather_by_city
 
 app = Flask(__name__)
 
@@ -9,31 +9,30 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('weather.html', data=[
-        {'name': 'Toronto'}, 
-        {'name': 'Montreal'}, 
-        {'name': 'Calgary'}, 
-        {'name': 'Ottawa'},
-        {'name': 'Edmonton'},
-        {'name': 'Mississauga'},
-        {'name': 'Winnipeg'},
-        {'name': 'Vancouver'},
-        {'name': 'Brampton'},
-        {'name': 'Quebec'}
-        ])
+        {'name': 'Manchester'},
+        {'name': 'Birmingham'},
+        {'name': 'London'},
+        {'name': 'Berlin'},
+        {'name': 'Porto'},
+        {'name': 'Helsinki'}
+    ])
+
 
 @app.route("/result", methods=['GET', 'POST'])
 def result():
     data = []
     error = None
     select = request.form.get('comp_select')
-    resp = query_api(select)
-    pp(resp) 
+    resp = get_weather_by_city(select)
+    pp(resp)
     if resp:
-        data.append(resp) 
+        data.append(resp)
     if len(data) != 2:
-        error = 'Bad Response from Weather API' 
+        error = 'Bad Response from Weather API'
         return render_template('result.html',
                                data=data,
                                error=error)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
