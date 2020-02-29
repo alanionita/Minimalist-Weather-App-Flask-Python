2247598928bfe702a97ca2232d2fea559c224b9e
Alan Ionita <a.g.ionita@gmail.com>
2020-02-29 20:25:20 +0000

    FIX : renames the weather.py file function to more descriptive names and updates the get_weather_by_city function to return data if the request is succesful

---
+++ api.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import os
+import pytz
+import math
+import requests
+
+API_KEY = os.environ.get("API_KEY")
+API_URL = ('http://api.openweathermap.org/data/2.5/weather?q={}&appid={}')
+
+
+def get_weather_by_city(city):
+    try:
+        print(API_URL.format(city, API_KEY))
+        response = requests.get(API_URL.format(city, API_KEY))
+        return response.json()
+    except Exception as exc:
+        print(exc)
+        data = None
+        return data
